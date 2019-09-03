de012895b16afd579964503fdf0cf7ce91c869fd
Nick Ganter <nickganter91@gmail.com>
2019-09-02 21:46:47 -0500

    added artificial work on thread to get to 100% for 5 minutes

--- Azure.Docker.Demo.Api/Controllers/ItemsController.cs
+++ Azure.Docker.Demo.Api/Controllers/ItemsController.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Threading;
 using Azure.Docker.Datalayer;
 using Azure.Docker.Datalayer.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +32,34 @@ namespace Azure.Docker.Demo.Api.Controllers
        public IActionResult Post(Item item)
        {
            _repo.SaveItem(item);
+           return Ok();
+       }
+
+       [HttpGet("item")]
+       public IActionResult GetItem()
+       {
+           return Ok(new Item 
+           {
+               Id = 999,
+               Name = "Test"
+           });
+       }
+
+       [HttpGet("work")]
+       public IActionResult Work()
+       {
+           //simulate 5 minutes of 100% work
+           var timer = new Stopwatch();
+           timer.Start();
+           while(true)
+           {
+               Thread.Sleep(1000);
+               if (timer.ElapsedMilliseconds == 300000)
+               {
+                   break;
+               }
+           }
+           timer.Stop();
            return Ok();
        }
     }
